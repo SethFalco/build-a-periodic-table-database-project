53832efb0865eb1ef50b1355e8d13c796d978f1d
SethFalco <seth@falco.fun>
2023-05-16 17:07:39 +0000

    feat: select data from database

--- element.sh
+++ element.sh
@@ -6,3 +6,12 @@ then
   echo 'Please provide an element as an argument.'
   exit
 fi
+
+if [[ $1 =~ [0-9]+ ]]
+then
+  WHERE="WHERE atomic_number = $1"  
+else
+  WHERE="WHERE symbol = '$1' OR name = '$1'"
+fi
+
+DATA=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) $WHERE;")
